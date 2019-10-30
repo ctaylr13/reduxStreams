1b422f05731486ef9e8fb9a7105cd734e69a35f7
ct <ctaylr13@gmail.com>
2019-10-29 21:21:43 -0400

    header added

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './Header';
 import StreamCreate from './streams/StreamCreate';
 import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
@@ -11,6 +12,7 @@ const App = () => {
         <div>
             <BrowserRouter>
                 <div>
+                    <Header />
                     <Route path="/" exact={true} component={StreamList} />
                     <Route path="/streams/new" exact={true} component={StreamCreate} />
                     <Route path="/streams/edit" exact={true} component={StreamEdit} />
