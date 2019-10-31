c26c67b5927dbb61df33dde419a6e6726c0b6d54
ct <ctaylr13@gmail.com>
2019-10-31 17:14:27 -0400

    streamedit path

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../apis/streams';
+import history from '../history';
 import { 
     SIGN_IN, 
     SIGN_OUT, 
@@ -27,8 +28,7 @@ export const createStream = formValues =>  async (dispatch, getState) => {
     const { userId } = getState().auth;
     const response = await streams.post('/streams', { ...formValues, userId });
     dispatch({ type: CREATE_STREAM, payload: response.data });
-    // do some programmatic navigation 
-    // get the user back to the root route
+    history.push('/'); //push is how we navigate users around 
 };
 
 export const fetchStreams = () => async dispatch => {
