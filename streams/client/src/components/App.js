6db6c048c31d4ff9b493db0a84ae36ae99836d78
ct <ctaylr13@gmail.com>
2019-11-01 16:36:15 -0400

    connect modal to delete button

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -17,7 +17,7 @@ const App = () => {
                     <Route path="/" exact={true} component={StreamList} />
                     <Route path="/streams/new" exact={true} component={StreamCreate} />
                     <Route path="/streams/edit/:id" exact={true} component={StreamEdit} />
-                    <Route path="/streams/delete" exact={true} component={StreamDelete} />
+                    <Route path="/streams/delete/:id" exact={true} component={StreamDelete} />
                     <Route path="/streams/show" exact={true} component={StreamShow} />
                 </div>
             </Router>
