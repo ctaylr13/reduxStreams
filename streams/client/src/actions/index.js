a62477e10dc0e50015863e60288ab94fdccb5013
ctaylor <ctaylr13@gmail.com>
2021-12-30 15:23:32 -0500

    update

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -1,14 +1,14 @@
 import streams from '../apis/streams';
 import history from '../history';
-import { 
-    SIGN_IN, 
-    SIGN_OUT, 
+import {
+    SIGN_IN,
+    SIGN_OUT,
     CREATE_STREAM,
     FETCH_STREAMS,
     FETCH_STREAM,
     DELETE_STREAM,
     EDIT_STREAM
- } from './types';
+} from './types';
 
 export const signIn = (userId) => {
     return {
@@ -24,7 +24,7 @@ export const signOut = () => {
 };
 
 // POST/create action, calls post, hits streams end point 
-export const createStream = formValues =>  async (dispatch, getState) => {
+export const createStream = formValues => async (dispatch, getState) => {
     const { userId } = getState().auth;
     const response = await streams.post('/streams', { ...formValues, userId });
     dispatch({ type: CREATE_STREAM, payload: response.data });
